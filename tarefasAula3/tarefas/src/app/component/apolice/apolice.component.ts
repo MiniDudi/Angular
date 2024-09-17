5155bb2904d1b68a7bd29eba63762602ab8f422d
MiniDudi <duduhnpinese@gmail.com>
2024-09-17 15:21:30 -0300

    feat: apolice and fix styles

--- tarefasAula3/tarefas/src/app/component/apolice/apolice.component.ts
+++ tarefasAula3/tarefas/src/app/component/apolice/apolice.component.ts
@@ -4,10 +4,18 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-apolice',
   templateUrl: './apolice.component.html',
-  styleUrl: './apolice.component.css'
+  styleUrls: ['./apolice.component.css']
 })
 export class ApoliceComponent {
+  valorApolice: number | null = null;
+
   onSubmit(form: NgForm) {
-    console.log(form.value)
+    const { nome, sexo, idade, valorAuto } = form.value;
+    
+    if (sexo === 'masculino') {
+      this.valorApolice = idade <= 25 ? valorAuto * 0.15 : valorAuto * 0.10;
+    } else if (sexo === 'feminino') {
+      this.valorApolice = valorAuto * 0.08;
+    }
   }
 }
